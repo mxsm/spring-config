fefa70fb5b1a9996dbe3f9419ebf9fdf7183fdfc
liujianbo <liujianbo@wbiao.cn>
2017-10-30 21:06:48 +0800

    add client

---
+++ config-client/src/main/java/com/github/mxsm/config/client/ConfigClient.java
@@ -0,0 +1,32 @@
+package com.github.mxsm.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author ant
+ * @Date 2017-10-30
+ * Dscription:
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClient 
+{
+	@Value("${profile}")
+	private String test;
+	
+	@RequestMapping("/")
+	public String get() {
+		
+		return test;
+	}
+	
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ConfigClient.class, args);
+    }
+}
